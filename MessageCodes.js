8590eb07aa84e73e5ae707a447808a840cabc3a6
Michael Liu <michael.liu0@gmail.com>
2020-08-23 18:15:58 -0700

    Initial Commit

---
+++ MessageCodes.js
@@ -0,0 +1,7 @@
+const NetworkErrorCode = Object.freeze({
+    CouldNotVerifyToken: 'Could not verify token',
+})
+
+module.exports = {
+    NetworkErrorCode
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Shared library for Krieg server and client
