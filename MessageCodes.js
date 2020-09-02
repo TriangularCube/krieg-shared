825d9e9c4079a5c3848c3ab298c9f3654d29d36c
Michael Liu <michael.liu0@gmail.com>
2020-09-01 18:28:33 -0700

    Add codes

--- MessageCodes.js
+++ MessageCodes.js
@@ -2,3 +2,8 @@ export const NetworkErrorCode = Object.freeze({
     CouldNotVerifyToken: 'Could not verify token',
     CouldNotVerifyLogin: 'Could not verify login',
 })
+
+export const GameSetupCode = Object.freeze({
+    Heartbeat: '--heartbeat--',
+    ConnectionDropped: '--connection dropped--',
+})
